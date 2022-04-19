a0609b2288f69b87e66cd6d116e81a9a927fa430
Mithaamalia13 <mthwza21@gmail.com>
2022-04-19 14:40:14 +0800

    2009106028_MithaAmalia_PostTest4

---
+++ 2009106028_MithaAmalia/src/posttest4/kelahiran.java
@@ -0,0 +1,48 @@
+package posttest4;
+
+public class kelahiran extends penduduk {
+    private long noLahir;
+    private String hariLahir, keterangan;
+    
+    kelahiran(long nik, long noLahir, String nama, String tempatLahir, String hariLahir, String tanggalLahir, String keterangan, String sex, String agama, String status, String pekerjaan, String alamat) {
+        super(nik, nama, tempatLahir, tanggalLahir, sex, agama, status, pekerjaan, alamat);
+        this.noLahir = noLahir;
+        this.hariLahir = hariLahir;
+        this.keterangan = keterangan;
+    }
+    
+    @Override //overreading
+    public void LihatData(){
+        System.out.println("NIK             : "+getNik());
+        System.out.println("No Kelahiran    : "+this.noLahir);
+        System.out.println("Nama            : "+getNama());
+        System.out.println("Tampat Lahir    : "+getTempatLahir());
+        System.out.println("Hari Lahir      : "+this.hariLahir);
+        System.out.println("Tanggal Lahir   : "+getTanggalLahir());
+        System.out.println("Keterangan      : "+this.keterangan);
+        System.out.println("Jenis Kelamin   : "+getSex());
+        System.out.println("Agama           : "+getAgama());
+        System.out.println("Status          : "+getStatus());
+        System.out.println("Pekerjaan       : "+getPekerjaan());
+        System.out.println("Alamat          : "+getAlamat());
+        
+    }
+    
+    //Overloading
+    public void LihatData(String status){
+        System.out.println("NIK             : "+getNik());
+        System.out.println("No Kelahiran    : "+this.noLahir);
+        System.out.println("Nama            : "+getNama());
+        System.out.println("Tampat Lahir    : "+getTempatLahir());
+        System.out.println("Hari Lahir      : "+this.hariLahir);
+        System.out.println("Tanggal Lahir   : "+getTanggalLahir());
+        System.out.println("Keterangan      : "+this.keterangan);
+        System.out.println("Jenis Kelamin   : "+getSex());
+        System.out.println("Agama           : "+getAgama());
+        System.out.println("Status          : "+getStatus());
+        System.out.println("Pekerjaan       : "+getPekerjaan());
+        System.out.println("Alamat          : "+getAlamat());
+        System.out.println("Status Data     : "+status);
+        
+    }
+}
---
+++ 2009106028_MithaAmalia/src/posttest4/kematian.java
@@ -0,0 +1,51 @@
+package posttest4;
+
+public class kematian extends penduduk {
+    private long noKematian;
+    private String tanggalKematian;
+    private String tempatKematian;
+    private String sebab;
+    
+    kematian(long nik, long noKematian, String nama, String tempatLahir, String tempatKematian, String tanggalLahir, String tanggalKematian, String sex, String agama, String status, String pekerjaan, String alamat, String sebab) {
+        super(nik, nama, tempatLahir, tanggalLahir, sex, agama, status, pekerjaan, alamat);
+        this.noKematian = noKematian;
+        this.tanggalKematian = tanggalKematian;
+        this.tempatKematian = tempatKematian;
+        this.sebab = sebab;
+    }
+    
+    @Override
+    public void LihatData(){
+        System.out.println("NIK                 : "+getNik());
+        System.out.println("No Kematian         : "+this.noKematian);
+        System.out.println("Nama                : "+getNama());
+        System.out.println("Tampat Lahir        : "+getTempatLahir());
+        System.out.println("Tempat Kematian     : "+this.tempatKematian);
+        System.out.println("Tanggal Lahir       : "+getTanggalLahir());
+        System.out.println("Tanggal Kematian    : "+this.tanggalKematian);
+        System.out.println("Jenis Kelamin       : "+getSex());
+        System.out.println("Agama               : "+getAgama());
+        System.out.println("Status              : "+getStatus());
+        System.out.println("Pekerjaan           : "+getPekerjaan());
+        System.out.println("Alamat              : "+getAlamat());
+        System.out.println("Sebab Kematian      : "+this.sebab);
+    }
+    
+    public void LihatData(String status){
+        System.out.println("NIK                 : "+getNik());
+        System.out.println("No Kematian         : "+this.noKematian);
+        System.out.println("Nama                : "+getNama());
+        System.out.println("Tampat Lahir        : "+getTempatLahir());
+        System.out.println("Tempat Kematian     : "+this.tempatKematian);
+        System.out.println("Tanggal Lahir       : "+getTanggalLahir());
+        System.out.println("Tanggal Kematian    : "+this.tanggalKematian);
+        System.out.println("Jenis Kelamin       : "+getSex());
+        System.out.println("Agama               : "+getAgama());
+        System.out.println("Status              : "+getStatus());
+        System.out.println("Pekerjaan           : "+getPekerjaan());
+        System.out.println("Alamat              : "+getAlamat());
+        System.out.println("Sebab Kematian      : "+this.sebab);
+        System.out.println("Status Data         : "+status);
+    }
+    
+}
