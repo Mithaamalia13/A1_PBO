b43d43e55f54fdd73e66c0f29900de6902957103
Mithaamalia13 <mthwza21@gmail.com>
2022-05-18 18:57:45 +0800

    2009106028_MithaAmalia_POSTTEST6

---
+++ 2009106028_MithaAmalia/src/posttest6/kecamatan.java
@@ -0,0 +1,61 @@
+package posttest6;
+
+public class kecamatan implements wilayah{
+    String namaKecamatan;
+    int noWilayah, jumlahPenduduk;
+    
+    public kecamatan(int noWilayah, String namaKecamatan, int jumlahPenduduk){
+        this.noWilayah = noWilayah;
+        this.namaKecamatan = namaKecamatan;
+        this.jumlahPenduduk = jumlahPenduduk;
+    }
+
+    @Override
+    public void wilayah1() {
+        System.out.println("No Wilayah      : "+this.noWilayah);
+        System.out.println("Nama Kecamatan  : "+this.namaKecamatan);
+        System.out.println("jumlah penduduk : "+this.jumlahPenduduk);
+        System.out.println("=======================================");
+    }
+
+    @Override
+    public void wilayah2() {
+        System.out.println("No Wilayah      : "+this.noWilayah);
+        System.out.println("Nama Kecamatan  : "+this.namaKecamatan);
+        System.out.println("jumlah penduduk : "+this.jumlahPenduduk);
+        System.out.println("=======================================");
+    }
+
+    @Override
+    public void wilayah3() {
+        System.out.println("No Wilayah      : "+this.noWilayah);
+        System.out.println("Nama Kecamatan  : "+this.namaKecamatan);
+        System.out.println("jumlah penduduk : "+this.jumlahPenduduk);
+        System.out.println("=======================================");
+    }
+
+    @Override
+    public void wilayah4() {
+        System.out.println("No Wilayah      : "+this.noWilayah);
+        System.out.println("Nama Kecamatan  : "+this.namaKecamatan);
+        System.out.println("jumlah penduduk : "+this.jumlahPenduduk);
+        System.out.println("=======================================");
+    }
+
+    @Override
+    public void wilayah5() {
+        System.out.println("No Wilayah      : "+this.noWilayah);
+        System.out.println("Nama Kecamatan  : "+this.namaKecamatan);
+        System.out.println("jumlah penduduk : "+this.jumlahPenduduk);
+        System.out.println("=======================================");
+    }
+
+    @Override
+    public void wilayah6() {
+        System.out.println("No Wilayah      : "+this.noWilayah);
+        System.out.println("Nama Kecamatan  : "+this.namaKecamatan);
+        System.out.println("jumlah penduduk : "+this.jumlahPenduduk);
+        System.out.println("=======================================");
+    }
+    
+}
---
+++ 2009106028_MithaAmalia/src/posttest6/wilayah.java
@@ -0,0 +1,11 @@
+package posttest6;
+
+public interface wilayah {
+    void wilayah1();
+    void wilayah2();
+    void wilayah3();
+    void wilayah4();
+    void wilayah5();
+    void wilayah6();
+    
+}
